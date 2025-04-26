5337739c96dc0d186f8d7d00ae9e7e8a61fdd050
Lucas Torquato <Lucas Torquato>
2025-04-26 11:14:42 -0300

    meu primeiro commit

---
+++ core/pessoa.py
@@ -0,0 +1,23 @@
+class Pessoa:
+    def __init__(self):
+        self.nome = ""
+        self.idade = 0
+
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, nome):
+        self.nome = nome
+
+    def get_idade(self):
+        return self.idade
+
+    def get_dados(self):
+        return self.nome, self.idade
+
+    def set_dados(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+
+
